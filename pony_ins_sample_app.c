2b5f4ea69125cf6f546580658eee75285a41a6bc
Alexander Kozlov <a.kozlov.msu@gmail.com>
2025-11-06 00:11:31 +0300

    Synchronize code with github.com/navlab-msu/inav-ins-sample-app

--- pony_ins_sample_app.c
+++ pony_ins_sample_app.c
@@ -1,3 +1,4 @@
+// Nov-2025
 /*
 	Пример приложения на архитектуре pony [https://github.com/p-o-n-y/pony], 
 	включая ядро и частные алгоритмы инерциальной навигации.
@@ -27,12 +28,12 @@
 #define PONY_INS_SAMPLE_APP_BUFFER_SIZE 1024
 
 // частные алгоритмы приложения
-void pony_ins_sample_app_step_sync     (void);
+void pony_ins_sample_app_time_step     (void);
 void pony_ins_sample_app_read_input    (void);
 void pony_ins_sample_app_write_output  (void);
 void pony_ins_sample_app_print_progress(void);
 
-void main(void)
+int main(void)
 {
 	const char pony_configuration[] = 
 		"{imu: lat = +55.0000302831, lon = +37.0000369619, alt = +200, alignment = 30, freq = 400, vertical_damping_stdev = 0}, sensors_in = wavesim.txt out = ins.nav";
@@ -40,7 +41,7 @@ void main(void)
 	printf("pony-ins-sample-app has started\n----\n");
 
 	// добавление частных алгоритмов
-	if (   !pony->add_plugin(pony_ins_sample_app_step_sync     ) // ожидание метки времени шага навигационного решения
+	if (   !pony->add_plugin(pony_ins_sample_app_time_step     ) // шаг времени
 		|| !pony->add_plugin(pony_ins_sample_app_read_input    ) // получение показаний датчиков
 		|| !pony->add_plugin(pony_ins_gravity_normal           ) // модель поля силы тяжести
 		|| !pony->add_plugin(pony_ins_alignment_static         ) // начальная выставка
@@ -51,7 +52,7 @@ void main(void)
 		|| !pony->add_plugin(pony_ins_sample_app_print_progress) // вывод на экран
 		) {
 		printf("ERROR on initialization\n"); // ошибка добавления частных алгоритмов
-		return;									
+		return -1;
 	}
 
 	// инициализация ядра
@@ -61,20 +62,21 @@ void main(void)
 	// ошибка инициализации
 	else {
 		printf("ERROR on initialization\n");
-		return;
+		return -2;
 	}
 
 	printf("\n----\npony-ins-sample-app has terminated\n");
+	return 0;
 }
 
 /*
-	шаг времени инерциального решения
+	шаг времени
 	использует:
-		не использует данные шины	
+		не использует данные шины
 	изменяет:
-		imu->t
-		imu->w_valid
-		imu->f_valid
+		pony->imu->t
+		pony->imu->w_valid
+		pony->imu->f_valid
 	параметры:
 		{imu: freq} — частота работы навигационного алгоритма, Гц
 			тип: число
@@ -82,10 +84,10 @@ void main(void)
 			значение по умолчанию: 100
 			пример: {imu: freq = 400}
 */
-void pony_ins_sample_app_step_sync(void) 
+void pony_ins_sample_app_time_step(void)
 {
 	const char   freq_token[] = "freq";     // имя параметра в строке конфигурации, содержащего частоту
-	const double freq_range[] = {50, 3200}; // диапазон допустимых частот
+	const double freq_range[] = {10, 3200}; // диапазон допустимых частот
 	const double freq_default = 100;        // частота по умолчанию
 
 	static double        dt = -1;           // шаг по времени
@@ -105,7 +107,7 @@ void pony_ins_sample_app_step_sync(void)
 		// поиск значения частоты в конфигурации
 		freq_ptr = pony_locate_token(freq_token, pony->imu->cfg, pony->imu->cfglength, '=');
 		if (freq_ptr != NULL)
-			dt = atof(freq_ptr);	
+			dt = atof(freq_ptr);
 		// если значение не найдено в конфигурации, или значение вне заданных пределов, установка по умолчанию
 		if (freq_ptr == NULL || dt < freq_range[0] || freq_range[1] < dt)
 			dt = freq_default;
@@ -118,7 +120,7 @@ void pony_ins_sample_app_step_sync(void)
 		// ничего не делать
 	}
 
-	// шаг основного цикла	
+	// шаг основного цикла
 	else {
 		// сброс достоверности инерциальных датчиков
 		pony->imu->w_valid = 0;
@@ -129,10 +131,10 @@ void pony_ins_sample_app_step_sync(void)
 	}
 }
 
-/*	
+/*
 	чтение показаний инерциальных датчиков из файла
 	использует:
-		не использует данные шины	
+		не использует данные шины
 	изменяет:
 		pony->imu.w
 		pony->imu.w_valid
@@ -149,7 +151,7 @@ void pony_ins_sample_app_read_input(void)
 {
 	const char input_file_token[] = "sensors_in";		  // имя параметра конфигурации с входным файлом
 	const int  n0 = 6;                                    // требуемое количество параметров в строке входного файла
-														  
+
 	static FILE *fp = NULL;                               // указатель на файл
 	static char  buffer[PONY_INS_SAMPLE_APP_BUFFER_SIZE]; // строковый буфер
 
@@ -221,7 +223,7 @@ void pony_ins_sample_app_read_input(void)
 	параметры:
 		out — имя выходного файла
 			тип: строка
-			пример: {imu: out = ins.txt }
+			пример: out = ins.nav
 			без пробелов в имени
 			с пробелом в конце
 */
@@ -254,7 +256,7 @@ void pony_ins_sample_app_write_output(void)
 			printf("ERROR opening output file '%s'\n", buffer);
 			pony->mode = -1;
 			return;
-		}		
+		}
 		// строка заголовка
 		fprintf(fp, "%%   time[s]||   lon[deg]  |  lat[deg]  |   alt[m]||   Ve[m/s]|   Vn[m/s]|   Vu[m/s]||    roll[deg]|  pitch[deg]| heading[deg]|");
 	}
@@ -269,7 +271,7 @@ void pony_ins_sample_app_write_output(void)
 	// операции на каждом шаге
 	else {
 		// вывод навигационного решения в файл
-		j = 0; 
+		j = 0;
 		//   time    lon    lat    alt    Ve    Vn    Vu    roll    pitch    yaw
 		                                         fprintf(fp, "\n% *.*f", fmt[j],fmt[j+1], pony->imu->t                                 ), j += 2;
 		fprintf(fp," "); for (i = 0; i < 2; i++) fprintf(fp, " % *.*f",  fmt[j],fmt[j+1], pony->imu->sol.llh[i]*pony->imu_const.rad2deg), j += 2;
@@ -289,9 +291,9 @@ void pony_ins_sample_app_write_output(void)
 		нет использует параметры
 */
 void pony_ins_sample_app_print_progress(void)
-{	
+{
 	const char bkspc[] = "\b\b\b\b\b"; // возврат курсора
-	const int 
+	const int
 		decimals = sizeof(bkspc)-1,    // количество выводимых десятичных знаков целой части
 		interval = 1024;               // интервал вывода, в шагах
 	static long counter;               // счётчик
